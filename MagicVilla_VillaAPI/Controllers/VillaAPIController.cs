da758fd117c87646679742edafe0fee785981a49
burak <burak@BURAKYIGIT>
2024-03-24 14:53:44 +0300

    VillaDTO added

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,22 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController:ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return new List<VillaDTO>
+            {
+                new VillaDTO{Id=0,Name="Pool View"},
+                new VillaDTO{Id=1,Name="Beach View"}
+            };
+        }
+    }
+}
